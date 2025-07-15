c5a67ee1ac10f9c924ee979a8f5c59a7094da43f
CodeWithJay-007 <pamnanijay4@gmail.com>
2025-07-15 23:07:00 +0530

    iife function

---
+++ iifeFunction.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expression
+
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('hitesh')
